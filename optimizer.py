0525ce2c5673389207ac0fa11d1fa422460a823e
EnsekiTT <ensekitt@gmail.com>
2017-01-02 03:39:56 +0900

    fix out_vector to l3

--- optimizer.py
+++ optimizer.py
@@ -5,7 +5,6 @@ def main():
     np.random.seed(10)
 
     in_vector = np.array([0.3, 0.5, 0.7, 0.2])
-    out_vector = np.array([0.0, 0.0, 0.0])
     teacher_vector = np.array([0.0, 0.0, 1.0])
 
     # Layer 1
@@ -22,20 +21,20 @@ def main():
 
     l1 = l1_h(l1_w.dot(in_vector))
     l2 = l2_h(l2_w.dot(l1))
-    out_vector = l3_h(l3_w.dot(l2))
-    print("Layer3 output: " + str(out_vector))
+    l3 = l3_h(l3_w.dot(l2))
+    print("Layer3 output: " + str(l3))
 
 
     # Loss Function
     lf = lambda y,t: -t * np.log(y) # multi cross entropy
 
     # Calc Loss
-    E = lf(out_vector, teacher_vector)
+    E = lf(l3, teacher_vector)
 
     # Back Propagation
     E_d = np.vectorize(lambda y,t: t/y) # d Cross entropy
     l3_h_d = np.vectorize(lambda x: 1) # d ReLU
-    l3_delta = l3_h_d(out_vector)*E_d(out_vector, teacher_vector)
+    l3_delta = l3_h_d(l3)*E_d(l3, teacher_vector)
 
     l2_h_d = np.vectorize(lambda x: 1 if x > 0 else 0) # d ReLU
     l2_delta = l2_h_d(l2)*l3_w.T.dot(l3_delta)
@@ -47,6 +46,7 @@ def main():
     in_delta = in_h_d(in_vector)*l1_w.T.dot(l1_delta)
 
     # Optimization(SGD)
+    l1_w = l1_w - alpha*in_vector*in_delta
 
 
 if __name__ == "__main__":
