0c320fd79a844dbef93d1584166ffff6e81713d1
EnsekiTT <ensekitt@gmail.com>
2017-01-02 03:28:13 +0900

    add back propagation

--- optimizer.py
+++ optimizer.py
@@ -29,7 +29,24 @@ def main():
     # Loss Function
     lf = lambda y,t: -t * np.log(y) # multi cross entropy
 
+    # Calc Loss
+    E = lf(out_vector, teacher_vector)
 
+    # Back Propagation
+    E_d = np.vectorize(lambda y,t: t/y) # d Cross entropy
+    l3_h_d = np.vectorize(lambda x: 1) # d ReLU
+    l3_delta = l3_h_d(out_vector)*E_d(out_vector, teacher_vector)
+
+    l2_h_d = np.vectorize(lambda x: 1 if x > 0 else 0) # d ReLU
+    l2_delta = l2_h_d(l2)*l3_w.T.dot(l3_delta)
+
+    l1_h_d = np.vectorize(lambda x: 1 if x > 0 else 0) # d ReLU
+    l1_delta = l1_h_d(l1)*l2_w.T.dot(l2_delta)
+
+    in_h_d = np.vectorize(lambda x: 1 if x > 0 else 0) # d ReLU
+    in_delta = in_h_d(in_vector)*l1_w.T.dot(l1_delta)
+
+    # Optimization(SGD)
 
 
 if __name__ == "__main__":
