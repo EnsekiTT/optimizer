b167bd7bd4faf560dffc612dd04bd550bb8bb269
EnsekiTT <ensekitt@gmail.com>
2017-01-11 18:14:41 +0900

    Chainerではうまく動いた。

--- chainer_ex.py
+++ chainer_ex.py
@@ -9,36 +9,22 @@ import chainer.functions as F
 import chainer.links as L
 from chainer import training
 from chainer.training import extensions
+from chainer.datasets import tuple_dataset
 
-"""
+
 class MLP(chainer.Chain):
-    def __init__(self, in_units, mid_units, n_out):
+    def __init__(self, in_units, n_out):
         super(MLP, self).__init__(
             l1=L.Linear(None, in_units),
-            l2=L.Linear(None, mid_units),
+            l2=L.Linear(None, in_units),
             l3=L.Linear(None, n_out),
         )
 
     def __call__(self, x):
         h1 = F.relu(self.l1(x))
         h2 = F.relu(self.l2(h1))
-"""
-
-# Network definition
-class MLP(chainer.Chain):
-
-    def __init__(self, n_units, n_out):
-        super(MLP, self).__init__(
-            # the size of the inputs to each layer will be inferred
-            l1=L.Linear(None, n_units),  # n_in -> n_units
-            l2=L.Linear(None, n_units),  # n_units -> n_units
-            l3=L.Linear(None, n_out),  # n_units -> n_out
-        )
-
-    def __call__(self, x):
-        h1 = F.relu(self.l1(x))
-        h2 = F.relu(self.l2(h1))
         return self.l3(h2)
+
 
 def iris_dataset():
     if not os.path.exists('iris.data'):
@@ -76,8 +62,8 @@ def main():
                         help='Number of units')
     args = parser.parse_args()
 
-    #model = L.Classifier(MLP(10, 10, 3))
-    model = L.Classifier(MLP(args.unit, 10))
+    #model = L.Classifier(MLP(1000, 1000, 3))
+    model = L.Classifier(MLP(args.unit, 3))
     if args.gpu >= 0:
         chainer.cuda.get_device(args.gpu).use()  # Make a specified GPU current
         model.to_gpu() # Copy the model to the GPU
@@ -86,55 +72,10 @@ def main():
     optimizer = chainer.optimizers.Adam()
     optimizer.setup(model)
 
-    # Load the MNIST dataset
-    train, test = chainer.datasets.get_mnist()
-
-    train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
-    test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
-        repeat=False, shuffle=False)
-
-    # Set up a trainer
-    updater = training.StandardUpdater(train_iter, optimizer, device=args.gpu)
-    trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
-
-    # Evaluate the model with the test dataset for each epoch
-    trainer.extend(extensions.Evaluator(test_iter, model, device=args.gpu))
-
-    # Dump a computational graph from 'loss' variable at the first iteration
-    # The "main" refers to the target link of the "main" optimizer.
-    trainer.extend(extensions.dump_graph('main/loss'))
-
-    # Take a snapshot at each epoch
-    trainer.extend(extensions.snapshot(), trigger=(args.epoch, 'epoch'))
-
-    # Write a log of evaluation statistics for each epoch
-    trainer.extend(extensions.LogReport())
-
-    # Print selected entries of the log to stdout
-    # Here "main" refers to the target link of the "main" optimizer again, and
-    # "validation" refers to the default name of the Evaluator extension.
-    # Entries other than 'epoch' are reported by the Classifier link, called by
-    # either the updater or the evaluator.
-    trainer.extend(extensions.PrintReport(
-        ['epoch', 'main/loss', 'validation/main/loss',
-         'main/accuracy', 'validation/main/accuracy', 'elapsed_time']))
-
-    # Print a progress bar to stdout
-    trainer.extend(extensions.ProgressBar())
-
-    if args.resume:
-        # Resume from a snapshot
-        chainer.serializers.load_npz(args.resume, trainer)
-
-    # Run the training
-    trainer.run()
-    exit()
-
     # Load the dataset
-    """
     iris = iris_dataset()
     iris = iris.reindex(np.random.permutation(iris.index))
-    iris_d = np.array(iris.iloc[0:150, 0:4])
+    iris_d = np.array(iris.iloc[0:150, 0:4], dtype=np.float32)
     iris_t = np.array(iris.iloc[0:150, 4:7])
     label = np.zeros(len(iris_t))
     for i, t in enumerate(iris_t):
@@ -144,11 +85,14 @@ def main():
             label[i] = 1
         if t[2] == 1.0:
             label[i] = 2
+    label = np.array(label, dtype=np.int32)
 
-    dataset = [(np.array(d,np.float32), int(label[i])) for i, d in enumerate(iris_d)]
-    train = dataset[0:100]
-    test = dataset[100:]
-    """
+    train = tuple_dataset.TupleDataset(iris_d[0:100], label[0:100])
+    test = tuple_dataset.TupleDataset(iris_d[100:150], label[100:150])
+
+    train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
+    test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
+        repeat=False, shuffle=False)
 
     # Set up a trainer
     updater = training.StandardUpdater(train_iter, optimizer, device=args.gpu)
